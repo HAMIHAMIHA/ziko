319a29281ce23e91d9e97a8491de876a2ba45345
Nina <nina@mediasia-interactive.com>
2021-09-26 18:56:43 +0800

    list offers

--- component/offers/offers.js
+++ component/offers/offers.js
@@ -8,16 +8,77 @@ Component({
 
   },
 
-  /**
-   * Component initial data
-   */
   data: {
-
+    offers: [
+      {
+        id: 1,
+        type: "kitchen",
+        lottery: true,
+        specials: true,
+        started: true,
+        startTime: "2021-09-21 14:00",
+        endTime: "2021-10-21 16:00",
+        items: 2
+      },
+      {
+        id: 11,
+        type: "cellar",
+        lottery: true,
+        specials: false,
+        started: true,
+        startTime: "2021-09-21 14:00",
+        endTime: "2021-10-21 16:00",
+        items: 1
+      },
+      {
+        id: 2,
+        type: "pet",
+        lottery: false,
+        specials: false,
+        started: true,
+        startTime: "2021-09-21 14:00",
+        endTime: "2021-10-21 16:00",
+        items: 0
+      },
+      {
+        id: 3,
+        type: "kitchen",
+        lottery: true,
+        specials: true,
+        started: false,
+        startTime: "2021-10-21 14:00",
+        endTime: "2021-10-21 16:00"
+      },
+      {
+        id: 3,
+        type: "kitchen",
+        lottery: false,
+        specials: false,
+        started: false,
+        startTime: "2021-10-21 14:00",
+        endTime: "2021-10-21 16:00"
+      },
+      {
+        id: 3,
+        type: "kitchen",
+        lottery: false,
+        started: false,
+        specials: true,
+        startTime: "2021-10-21 14:00",
+        endTime: "2021-10-21 16:00"
+      },
+      {
+        id: 4,
+        type: "cellar",
+        lottery: true,
+        specials: true,
+        started: false,
+        startTime: "2021-10-21 14:00",
+        endTime: "2021-10-21 16:00"
+      }
+    ]
   },
 
-  /**
-   * Component methods
-   */
   methods: {
 
   }
