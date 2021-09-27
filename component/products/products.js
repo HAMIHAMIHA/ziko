50f4f5de90a2b24d324b919f013238d2c5d4dd83
Nina <nina@mediasia-interactive.com>
2021-09-27 18:41:24 +0800

    ziko special in product detail

--- component/products/products.js
+++ component/products/products.js
@@ -3,24 +3,14 @@ Component({
   properties: {
     products: Array,
     pack: Boolean,
-    cellar: Boolean,
     targets: Array,
-    targetType: String
+    targetType: String,
+    units: String
   },
 
-  data: {
-    units: 'g'
-  },
   methods: {
 
   },
-  ready: function() { 
-    const self = this;
-    console.log(self.data.products);
-    if (self.data.cellar) {
-      self.setData({
-        units: "cl"
-      })
-    }
+  ready: function() {
   }
 })
--- pages/offer/offer.js
+++ pages/offer/offer.js
@@ -3,7 +3,8 @@ Page({
     _pageSet: {
       swiperIndex: 1,
       nextOffer: 'test',
-      currentTab: "product"
+      currentTab: "product",
+      units: "g"
     },
     _offer: {
       community: 'cellar',
@@ -65,37 +66,64 @@ Page({
         }],
         price: 500
       }],
-      singles: [
-        {
-          id: 1,
-          available: 31,
-          quantity: 2,
-          weight: 200,
-          price: 500,
-          newPrice: 200
-        },
-        {
+      singles: [{
+        id: 1,
+        available: 31,
+        quantity: 2,
+        weight: 200,
+        price: 500,
+        newPrice: 200
+      }, {
+        id: 2,
+        available: 190,
+        quantity: 2,
+        weight: 200,
+        price: 500
+      }, {
+        id: 3,
+        available: 0,
+        quantity: 2,
+        weight: 200,
+        price: 500
+      }, {
+        id: 4,
+        available: 0,
+        quantity: 2,
+        weight: 200,
+        price: 500
+      }],
+      specials: [{
+        type: "first",
+        amount: 20,
+        get: "addon",
+        addon: {
           id: 2,
           available: 190,
           quantity: 2,
           weight: 200,
           price: 500
-        },
-        {
-          id: 3,
-          available: 0,
-          quantity: 2,
-          weight: 200,
-          price: 500
-        },
-        {
-          id: 3,
-          available: 0,
-          quantity: 2,
-          weight: 200,
-          price: 500
         }
-      ]
+      }, {
+        type: "totalAbove",
+        amount: 20,
+        get: "reduction",
+        rate: "10"
+      }, {
+        type: "orderAbove",
+        amount: 20,
+        get: "reduction",
+        rate: "10"
+      }, {
+        type: "totalSold",
+        amount: 20,
+        get: "reduction",
+        rate: "10"
+      }, {
+        type: "cart",
+        item: "pack a",
+        get: "reduction",
+        rate: "10"
+      }]
     },
     messages: [
       "message1",
@@ -108,6 +136,16 @@ Page({
       "message mesag asg asgkljsd agl message2",
       "mesag asg asgkljsd aessage2"
     ]
+  },
+
+  onLoad: function() {
+    const self = this;
+    console.log(self.data.products);
+    if (self.data._offer.community) {
+      self.setData({
+        "_pageSet.units": "cl"
+      })
+    }
   },
 
   swiperChange: function(e) {
