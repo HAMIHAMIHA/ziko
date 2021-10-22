b87843d9c23a60abd51c6f02aaf1c62677dc4504
Nina <nina@mediasia-interactive.com>
2021-10-22 14:37:29 +0800

    address list style

--- pages/address/index/index.js
+++ pages/address/index/index.js
@@ -1,66 +1,16 @@
-// pages/address/index/index.js
+const app = getApp();
+const _routes = app.routes;
+
 Page({
-
-  /**
-   * Page initial data
-   */
   data: {
-    address: [1, 2]
+    address: [
+      {id: 1},
+      {id: 2}
+    ],
+    _routes: _routes
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function () { }
 })
