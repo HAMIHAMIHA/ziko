6124f434e537aa0e2e5374895bac88c0cc6065cf
Nina <nina@mediasia-interactive.com>
2021-12-21 17:33:34 +0800

    change default language

--- utils/translate.js
+++ utils/translate.js
@@ -1,26 +1,21 @@
 const db = require('db.config.js');
 
 const checkLanguage = () => {
-  wx.getSystemInfo({
-    success(res) {
-      if (res.language == "zh_CN") {
-        db.set('language', res.language);
+  if (!db.get('language')) {
+    db.set('language', "en");
+    wx.getSystemInfo({
+      success(res) {
+        if (res.language == "zh_CN") {
+          db.set('language', "zh_CN");
+        }
       }
-    }
-  })
-
-  // db.set('language', "en"); // If no language, set to en
-  db.set('language', "zh_CN"); // TEMP if no language, set to en
-  // on app load if user logged in => set user language as lang
-  // else set wechat setting language as lang
-
-  // on user login => check if language was set, if not empty and not equals to current change to user setting language
-  // if empty set user language to lang
+    })
+  }
 }
 
 const changeLanguage = (language) => {
-    db.set('language', language)
-    return translate();
+  db.set('language', language)
+  return translate();
 }
 
 // get translation file
