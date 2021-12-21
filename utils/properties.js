2e3f30d9e820deea523abe2c1191d6de86b4b13a
Nina <nina@mediasia-interactive.com>
2021-12-21 17:35:10 +0800

    add defult url

--- utils/properties.js
+++ utils/properties.js
@@ -1,3 +1,11 @@
+const API_URL = 'https://api-ziko.dev.mediasia.cn/';
+const FILE_URL = `${API_URL}storage/`;
+
 module.exports = {
-  api_url: 'https://api-atelier-fusari.dev.mediasia.cn/'
+  api_url: API_URL,
+  folders: {
+    // category: `${FILE_URL}product-category/`,
+    // product_main: `${FILE_URL}product-main-picture/`,
+    // product_others: `${FILE_URL}product-other-picture/`
+  }
 }
