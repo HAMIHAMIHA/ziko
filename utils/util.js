1329cd2f2fd0a01a4045ba9904dc61f26a0b9092
Nina <nina@mediasia-interactive.com>
2021-12-21 12:42:47 +0800

    add find index util

--- utils/util.js
+++ utils/util.js
@@ -1,14 +1,14 @@
 const formatNumber = n => {
-  n = n.toString()
+  n = n.toString();
   return n[1] ? n : `0${n}`
 }
 
 // Output date to yyyy.mm.dd format
 const formatDate = dateLong => {
-  const date = new Date(dateLong)
-  const year = date.getFullYear()
+  const date = new Date(dateLong);
+  const year = date.getFullYear();
   const month = date.getMonth() + 1
-  const day = date.getDate()
+  const day = date.getDate();
 
   return [year, month, day].map(formatNumber).join('.')
 }
@@ -23,17 +23,19 @@ const formatTime = dateLong => {
   return [hour, minute, seconds].map(formatNumber).join(':')
 }
 
-const formatTimer = dateLong => {
-  let time = dateLong / 1000;
-
-  // let day = parseInt(time / (60 * 60 * 24));
-  let hour = parseInt(time % (60 * 60 * 24) / 3600);
-  let minute = parseInt(time % (60 * 60 * 24) % 3600 / 60);
-  let seconds = parseInt(time % (60 * 60 * 24) % 3600 % 60);
-
-  return [hour, minute, seconds].map(formatNumber).join(':')
-}
-
 module.exports = {
-  formatTimer
+  findIndex: (list, find, key) => {
+    return list.findIndex(item => item[key] == find);
+  },
+
+  formatTimer: dateLong => {
+    let time = dateLong / 1000;
+  
+    // let day = parseInt(time / (60 * 60 * 24));
+    let hour = parseInt(time % (60 * 60 * 24) / 3600);
+    let minute = parseInt(time % (60 * 60 * 24) % 3600 / 60);
+    let seconds = parseInt(time % (60 * 60 * 24) % 3600 % 60);
+  
+    return [hour, minute, seconds].map(formatNumber).join(':')
+  }
 }
