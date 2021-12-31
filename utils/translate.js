bd6c4632384011b3b600861781cffd1f9db00130
Nina <nina@mediasia-interactive.com>
2021-12-31 12:21:38 +0800

    update language setting

--- utils/translate.js
+++ utils/translate.js
@@ -1,16 +1,20 @@
 const db = require('db.config.js');
 
 const checkLanguage = () => {
-  if (!db.get('language')) {
-    db.set('language', "en");
-    wx.getSystemInfo({
-      success(res) {
-        if (res.language == "zh_CN") {
-          db.set('language', "zh_CN");
-        }
+  // Check if language in cache
+  if (db.get('language')) return;
+
+  // Default language is English
+  db.set('language', "en");
+
+  // If system language is in Chinese, switch language
+  wx.getSystemInfo({
+    success(res) {
+      if (res.language == "zh_CN") {
+        db.set('language', "zh_CN");
       }
-    })
-  }
+    }
+  })
 }
 
 const changeLanguage = (language) => {
@@ -20,6 +24,7 @@ const changeLanguage = (language) => {
 
 // get translation file
 const translate = () => {
+  // TODO change langaue map
   console.log(db.get('language'));
   return require('../i18n/' + db.get('language') + '.js').languageMap;
 }
