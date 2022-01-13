fdc483b3cd3d55a8e7557ea14ea2f73df7222bb8
Nina <nina@mediasia-interactive.com>
2022-01-13 12:20:32 +0800

    redo api offers in index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -26,6 +26,46 @@ const _timerControl = (page, timer_switch) => {
   }
 }
 
+// Rules for generating api url
+const _generateSuffix = (step, filter_date) => {
+  const start_suffix = `&filter={"$and":[{"$or":[{"channel":"all"},{"channel":"miniprogram"},{"channel":"wechat_group"}]}`;
+
+  let key = 'general';
+  let now = new Date();
+  if (filter_date) {
+    let today = now.setHours(0, 0, 0, 0);
+    key = (filter_date == today) ? (filter_date > today) ? 'tomorrow' : 'yesturday' : 'today';
+  }
+
+  const filter_str_list = {
+    general: {
+      1: [`,{"endingDate":{"$gte":"${ now }"}},{"startingDate":{"$lte":"${ now }"}}`, `["endingDate","ASC"]`],
+      2: [`,{"startingDate":{"$gt":"${ now }"}},{}`, `["startingDate","ASC"]`]
+    },
+    today: {
+      // TODO
+      // filter_date.setHours(0, 0, 0, 0) < startingDate < now and now < endingDate => 1
+      // startingDate > now and startingDate < filter_date.setHours(999) => 2
+
+      // 1: [`,{"endingDate":{"$gte":"${ now }"}},{"startingDate":{"$lte":"${ now }"}}`, `["endingDate","ASC"]`],
+      // 2: [`,{"startingDate":{"$gt":"${ now }"}},{}`, `["startingDate","ASC"]`]
+    },
+    tomorrow: {
+      // TODO
+      // filter_date.setHours(0, 0, 0, 0) < startingDate < filter_date.setHours(999) => 1
+      // 1: [`,{"endingDate":{"$gte":"${ now }"}},{"startingDate":{"$lte":"${ now }"}}`, `["endingDate","ASC"]`]
+    },
+    yesturday: {
+      // TODO
+      // filter_date.setHours(0, 0, 0, 0) < startingDate < filter_date.setHours(999) and endingDate > now => 1
+      // 1: [`,{"endingDate":{"$gte":"${ now }"}},{"startingDate":{"$lte":"${ now }"}}`, `["endingDate","ASC"]`],
+    }
+  }
+
+  let filter_str = filter_str_list[key][step];
+  return filter_str ? `${ start_suffix }${ filter_str[0] }]}&sort=${ filter_str[1] }` : null;
+}
+
 // Get offer data by filters
 const _filterOfferData = (page, filter_type, filter_group, filter_id, filter_date) => {
   let suffix = '';
@@ -62,21 +102,28 @@ const _filterOfferData = (page, filter_type, filter_group, filter_id, filter_dat
 
   // Loading module
   showLoading(true);
+  
+  let raw_offers = [];
 
   // Set up page data, Start new timers, Change date filters
   let callback = {
     success: res => {
+      raw_offers = raw_offers.concat(res);
+
       let offers = [];
       let days = page.data.days;
       if (!filter_date) {
         days = []; // create list for date picker
       }
+      console.log(res);
 
-      for (var i in res) {
-        let offer = res[i];
+      for (var i in raw_offers) {
+        let offer = raw_offers[i];
+        // console.log(offer.startingDate);
+        // console.log(offer.endingDate);
         offer.started = (new Date() >= new Date(offer.startingDate));
 
-        let date_value = formatWeekDate(res[i].startingDate);
+        let date_value = formatWeekDate(offer.startingDate);
 
         if (!filter_date && findIndex(days, date_value.timestamp, "timestamp") == -1) {
           days.push(date_value);
@@ -96,17 +143,26 @@ const _filterOfferData = (page, filter_type, filter_group, filter_id, filter_dat
     }
   };
 
-  // Set up API
-  let general_filter = `&filter={"$and":[{"$or":[{"channel":"all"},{"channel":"miniprogram"},{"channel":"wechat_group"}]},{"endingDate":{"$gte":"${ new Date() }"}},{}]}`;
+  // Get on going events
+  let firstCallback = {
+    success: res => {
+      console.log(res);
+      raw_offers = res;
 
-  // TODO get filterable dates
-  if (filter_date) {
-    let starting_date_filter = `{"startingDate":{"$gte":"${ new Date(filter_date).setHours(0, 0, 0, 0) }", "$lte": "${ new Date(filter_date).setHours(23,59,59,999) }"}}`;
-    general_filter.replace('{}', starting_date_filter);
+      // Set up API
+      suffix = `?community=${ filter_id }${ _generateSuffix(2, filter_date) }`;
+
+      if (suffix) {
+        app.api.getOffers(suffix, callback);
+      } else {
+        callback.success([]);
+      }
+    }
   }
 
-  suffix = `?community=${filter_id}${general_filter}`;
-  app.api.getOffers(suffix, callback);
+  // Set up API
+  suffix = `?community=${filter_id}${ _generateSuffix(1, filter_date) }`;
+  app.api.getOffers(suffix, firstCallback);
 }
 
 Page({
