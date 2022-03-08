c94cea1ec5d1b8b491a0dbf1292987634b7635b4
Nina <nina@mediasia-interactive.com>
2022-03-08 16:20:34 +0800

    fix typo

--- pages/offer/regular/regular.js
+++ pages/offer/regular/regular.js
@@ -57,7 +57,7 @@ Page({
     })
   },
 
-  // Switch between reciepe and products
+  // Switch between recipe and products
   switchTab: function(e) {
     const self = this;
     // TODO change to show and hide css on switch
