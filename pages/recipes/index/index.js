95f7bb70bd40d73ac7fa32a155fd0c26527d8997
Nina <nina@mediasia-interactive.com>
2022-03-08 16:20:56 +0800

    page translation

--- pages/recipes/index/index.js
+++ pages/recipes/index/index.js
@@ -25,6 +25,26 @@ Page({
   onShow: function() {
     const self = this;
     _setTranslation(self);
+
+    updatePageLanguage();
+  },
+
+  updatePageLanguage: function() {
+    const self = this;
+
+    // Translate tabbar
+    app.setTabbar();
+
+    // Translation and default values
+    let i18n = app.globalData.i18n;
+    self.setData({
+      _language: app.db.get('language'),
+      _t: {
+        baking: i18n.baking,
+        min: i18n.min,
+        preparation: i18n.preparation,
+      }
+    })
   },
 
   changeRecipes: function() {
