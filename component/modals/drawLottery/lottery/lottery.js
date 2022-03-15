7f68ba90075a943e7fadc13738b6bcd5d64a242b
Nina <nina@Nina2016MP.local>
2022-03-15 17:43:48 +0800

    lottery popups

---
+++ component/modals/drawLottery/lottery/lottery.js
@@ -0,0 +1,27 @@
+// component/modals/drawLottery/lottery/lottery.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    showResult: function(e) {
+      const self = this;
+
+      // TODO get components show sorry or win
+    }
+  }
+})
---
+++ component/modals/drawLottery/win/win.js
@@ -0,0 +1,23 @@
+// component/modals/draw/draw.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
