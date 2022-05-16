2155d8e514c407d78a8b18f3323c701c49ac223e
Nina <nina@Nina2016MP.local>
2022-05-16 09:55:45 +0800

    close sorry popup

--- component/modals/drawLottery/sorry/sorry.js
+++ component/modals/drawLottery/sorry/sorry.js
@@ -2,8 +2,8 @@ const app = getApp();
 Component({
   data: {
     _t: {
-      back_to_order: app.globalData.i18n.back_to_order,
       better_luck_next_time: app.globalData.i18n.better_luck_next_time,
+      close_it: app.globalData.i18n.close_it,
       has_won_the_lottery: app.globalData.i18n.has_won_the_lottery,
       not_this_time: app.globalData.i18n.not_this_time,
       sorry: app.globalData.i18n.sorry,
@@ -38,11 +38,8 @@ Component({
       })
     },
 
-    toOrder: function() {
+    closeModal: function() {
       this.selectComponent('#modal_template').closeModal();
-      wx.switchTab({
-        url: app.routes.orders
-      })
     }
   }
 })
