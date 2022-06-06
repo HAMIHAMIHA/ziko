b7cd69eefe0f53d0ebb112ac9f8632b09f1749da
Nina <nina@mediasia-interactive.com>
2022-06-06 11:32:19 +0800

    fapiao info from wechat

--- pages/account/fapiao/fapiao.js
+++ pages/account/fapiao/fapiao.js
@@ -2,7 +2,14 @@ const { refreshUserInfo, updateUserInfo, showLoading } = require("../../../utils
 
 const app = getApp();
 
+let need_refresh = true; // Used to prevent refresh user info when adding wechat fapiao info
+let get_fapiao = true; // Used to enable get fapiao info from Wechat
+
 Page({
+  onLoad: function() {
+    need_refresh = true;
+  },
+
   onShow: function () {
     let self = this;
     let i18n = app.globalData.i18n;
@@ -12,12 +19,16 @@ Page({
       title: i18n.fapiao
     })
 
-    refreshUserInfo(null, res =>{
-      self.setData({
-        fapiao: res.fapiaoInformation ? res.fapiaoInformation : '',
-      })
-      showLoading(false);
-    });
+    // Refreshing user info
+    if (need_refresh) {
+      refreshUserInfo(null, res =>{
+        self.setData({
+          fapiao: res.fapiaoInformation ? res.fapiaoInformation : '',
+        })
+        showLoading(false);
+        need_refresh = false;
+      });
+    }
 
     // Restart lottery popup
     app.globalData.pause_lottery_check = false;
@@ -29,6 +40,40 @@ Page({
         save: i18n.save,
       }
     })
+  },
+
+  // Get Wechat fapiao info
+  getFapiaoInfo: function() {
+    const self = this;
+    if (!get_fapiao) return;
+    wx.chooseInvoiceTitle({
+      success: res => {
+        let fapiao_info = `${ res.title }\n${ res.taxNumber }\n${ res.companyAddress }`;
+
+        self.setData({
+          fapiao: fapiao_info
+        })
+      },
+
+      complete: res => {
+        self.setData({
+          _focus: true
+        })
+        get_fapiao = false;
+      }
+    })
+  },
+
+  // Change fapiao in page data
+  setFapiaoInfo: function(e) {
+    this.setData({
+      fapiao: e.detail.value
+    })
+  },
+
+  // Enable get fapiao info from Wechat
+  unsetFocus: function() {
+    get_fapiao = true;
   },
 
   // Save data
