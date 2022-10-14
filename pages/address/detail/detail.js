f7d00aaec22ca406778ffb406f11c4bee2db9ada
Ryland <ryland@slingshot.sh>
2022-10-14 17:21:14 +0800

    feat: [address_detail] add address

--- pages/address/detail/detail.js
+++ pages/address/detail/detail.js
@@ -1,5 +1,5 @@
-const { changeFocus, navigateBack, showLoading } = require("../../../utils/common.js");
-const { findIndex } = require("../../../utils/util.js");
+const {changeFocus, navigateBack, showLoading} = require("../../../utils/common.js");
+const {findIndex} = require("../../../utils/util.js");
 
 const app = getApp();
 const address_type = ["office", "home", "other"];
@@ -12,8 +12,13 @@ let all_areas = [];
 
 const _getAddressAreas = (page, area_id) => {
   app.api.getAreas().then(res => {
+    const area = res[findIndex(res, area_id, 'id')];
     page.setData({
-      area: res[findIndex(res, area_id, 'id')]
+      area,
+      province: {
+        value: area.id,
+        label: area.name,
+      }
     })
   });
 }
@@ -50,7 +55,7 @@ async function getUserInfo(page) {
 // Check if input empty
 const _validateInputs = (page, data) => {
   let error = '';
-  console.log("validate_keys",validate_keys,"data",data)
+  console.log("validate_keys", validate_keys, "data", data)
   for (var i in validate_keys) {
     console.log(i, data[validate_keys[i]]);
     (data[validate_keys[i]] == null || data[validate_keys[i]] === '') ? error += `error-field-${i} ` : '';
@@ -66,7 +71,7 @@ const _validateInputs = (page, data) => {
   page.setData({
     error: error
   })
-  console.error("show-error",error)
+  console.error("show-error", error)
   return error;
 }
 
@@ -90,68 +95,52 @@ const _generateUserAddress = (page, action, new_address) => {
   return address;
 }
 
-  //Get Province
-  const _getAreas = (parent_id) => {//__need
-    let new_areas = [];
-    const filtered_areas = all_areas.filter((e) => e.parent === parent_id);
-    if (filtered_areas.length) {
-      for (const line of filtered_areas) {
-        let childs = _getAreas(line.id)
-        new_areas.push({
-          value: line.id,
-          label: line.name,
-          children: childs.length ? childs : undefined,
-        })
-      }
-    }
-    return new_areas;
-  }
-  const _formatAreas = (areas, index) => {
-    if (index > 1) {
-      for (const item of areas) {
-        if (item.children && "length" in item.children ) _formatAreas(item.children, index - 1)
-        else item.children = [{
-          value: item.value,
-          label: item.label
-        }]
-      }
-    }
-  }
-
-  const _find_current = (current_id) => {
-    if (!current_id) return [0];
-    // Find area and all ancestors
-    let selected_rev = [all_areas[findIndex(all_areas, current_id, 'id')]];
-    let area = selected_rev[0];
-    while (area.parent) {
-      selected_rev.push(all_areas[findIndex(all_areas, area.parent, 'id')]);
-      area = selected_rev[selected_rev.length - 1];
-    }
-    // Find index of all selected options
-    let selected = [];
-    for (var i = selected_rev.length - 1; i >= 0; i--) {
-      const filtered_areas = all_areas.filter((e) => e.parent === selected_rev[i].parent)
-      selected.push(findIndex(filtered_areas, selected_rev[i].id, 'id'));
-    }
-    return selected;
-  }
-
-  const getAddressAreaList = page => {//__need
-    const callback = res => {
-      all_areas = res;
-      let areas = _getAreas();
-      _formatAreas(areas, 3);
-      console.log("details_areas:",JSON.stringify(areas));
-
-      page.setData({
-        _temp : res,
-        _areas: areas,
-        _selected: _find_current(page.options.id) // TEMP need for when already selected
+//Get Province
+const _getAreas = (parent_id) => {//__need
+  let new_areas = [];
+  const filtered_areas = all_areas.filter((e) => e.parent === parent_id);
+  if (filtered_areas.length) {
+    for (const line of filtered_areas) {
+      let childs = _getAreas(line.id)
+      new_areas.push({
+        value: line.id,
+        label: line.name,
+        children: childs.length ? childs : undefined,
       })
-      _loadPicker(page);
     }
-    app.api.getAreas().then(callback);
   }
+  return new_areas;
+}
+const _formatAreas = (areas, index) => {
+  if (index > 1) {
+    for (const item of areas) {
+      if (item.children && "length" in item.children) _formatAreas(item.children, index - 1)
+      else item.children = [{
+        value: item.value,
+        label: item.label
+      }]
+    }
+  }
+}
+
+
+const getAddressAreaList = page => {//__need
+  console.log("getAddressAreaList")
+  const callback = res => {
+    all_areas = res;
+    let areas = _getAreas();
+    _formatAreas(areas, 3);
+    console.log("details_areas:", JSON.stringify(areas));
+
+    page.setData({
+      _temp: res,
+      _areas: areas,
+    })
+    _loadPicker(page);
+  }
+  app.api.getAreas().then(callback);
+}
+
 function _loadPicker(page) {
   let state = {
     arr: [],
@@ -199,8 +188,8 @@ Page({
     multiIds: [],
     newArr: [],
   },
-  onLoad: function () {
-  // onShow: function () {
+  onShow: function () {
+    // onShow: function () {
     let self = this;
     let i18n = app.globalData.i18n;
 
@@ -242,14 +231,14 @@ Page({
         zipcode: i18n.zipcode,
         cancel: i18n.cancel,
         province: i18n.province,
-        any_special_requests:i18n.any_special_requests,
-        please_select:i18n.please_select,
-        please_select_your_province:i18n.please_select_your_province,
-        please_select_your_city:i18n.please_select_your_city,
-        please_enter_your_address:i18n.please_enter_your_address,
-        please_enter_your_zip_code:i18n.please_enter_your_zip_code,
-        any_special_requests:i18n.any_special_requests,
-        options_judge:self.options.id,
+        any_special_requests: i18n.any_special_requests,
+        please_select: i18n.please_select,
+        please_select_your_province: i18n.please_select_your_province,
+        please_select_your_city: i18n.please_select_your_city,
+        please_enter_your_address: i18n.please_enter_your_address,
+        please_enter_your_zip_code: i18n.please_enter_your_zip_code,
+        any_special_requests: i18n.any_special_requests,
+        options_judge: self.options.id,
         select: i18n.select,
         set_default_address: i18n.set_as_default_address,
       },
@@ -262,7 +251,7 @@ Page({
   },
 
   // Change picker result
-  bindPickerChange: function(e) {
+  bindPickerChange: function (e) {
     const self = this;
 
     let new_index = e.detail.value;
@@ -274,12 +263,12 @@ Page({
   },
 
   // Change input focus
-  next: function(e) {
+  next: function (e) {
     changeFocus(this, e);
   },
 
   // Save address info
-  updateAddress: function(e) {
+  updateAddress: function (e) {
     const self = this;
     let action = e.type; //提交类型
     console.log(action)
@@ -297,22 +286,24 @@ Page({
 
     // Add address to addrss list
     // let address = e.detail.value;
-    const { detailedAddress, type, zipCode } = e.detail.value;
-    const city = this.data;
-    const defaultData = this.data.default;
-    const addressType = this.data._picker.address_type[type];
-    const address = {detailedAddress, type: addressType, zipCode, city, default: defaultData}
+    const {detailedAddress, type, zipCode, city} = e.detail.value;
+    const {default: defaultData, _picker: {address_type}, province} = this.data;
+    const addressType = address_type[type]?.toLowerCase();
+    const area = province.value;
+    const address = {area, detailedAddress, type: addressType, zipCode, city, default: defaultData}
     console.log(address, "address")
     // address ? address.type = self.data.address.type : '';
     // address ? address.area = self.data.area.id : '';
     let address_list = _generateUserAddress(self, action, address);
     console.log("address_list", address_list)
 
-    app.sessionUtils.updateUserInfo({ addresses: address_list }, app.routes.address);
+    app.sessionUtils.updateUserInfo({addresses: address_list}, app.routes.address);
+    showLoading(false);
+    wx.navigateTo(app.routes.address);
   },
 
 
-  select: function(e) {//__need
+  select: function (e) {//__need
     const self = this;
 
     let selected = self.data._selected;
@@ -325,7 +316,7 @@ Page({
 
     // Set area data to address detail page
     let pages = getCurrentPages();
-    let prev = pages[pages.length -2];
+    let prev = pages[pages.length - 2];
 
     prev.setData({
       area: all_areas[findIndex(all_areas, area.value, 'id')]
@@ -341,7 +332,7 @@ Page({
       delta: 1,
     })
   },
-  setDefault: function() {
+  setDefault: function () {
     const self = this;
 
     self.setData({
